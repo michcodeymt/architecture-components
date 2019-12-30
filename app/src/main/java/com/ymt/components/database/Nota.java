19a1967582ca1ac64ec904d89ab29a085aeb3592
Yuri Montañez <mmontanezt@gmail.com>
2019-12-30 18:35:13 -0500

    first commit

---
+++ app/src/main/java/com/ymt/components/database/Nota.java
@@ -0,0 +1,50 @@
+package com.ymt.components.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+/**
+ * @author michcode
+ */
+@Entity(tableName = "tabla_notas")
+public class Nota {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    private int id;
+
+    @ColumnInfo(name = "titulo")
+    private String titulo;
+
+    @ColumnInfo(name = "descripcion")
+    private String descripcion;
+
+    public Nota() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
